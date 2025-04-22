a5f557882a646f349df01aca0f1da74e69e15c58
Prabin Joshi <joshiprabin17@gmail.com>
2025-04-22 19:57:53 +0545

    Refactor BannerImage component for improved responsiveness; update image properties for better performance

--- components/website/BannerImage.tsx
+++ components/website/BannerImage.tsx
@@ -3,13 +3,15 @@ import React from 'react'
 
 export default function BannerImage() {
     return (
-        <div className="relative w-full h-72 ">
+        <div className="relative w-full h-45 sm:h-48 md:h-56 lg:h-64 xl:h-72">
             <Image
                 src="/banner.png"
                 alt="Banner"
-                layout="fill"
-                objectFit="cover"
+                fill
+                sizes="100vw"
                 priority
+                quality={80}
+                className="object-cover" // Ensures the image covers the area properly
             />
         </div>
     )
